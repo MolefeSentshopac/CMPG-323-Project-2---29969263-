23c2c9f85ef89a51ec89edb3c0b8858e8d5e6f4e
Molefe <67503220+MolefeSentshopac@users.noreply.github.com>
2022-09-08 04:03:43 +0200

    <script src="https://gist.github.com/ankitvijay/21b45c70e00846d5ce738005fae0372b.js"></script>

---
+++ PROJECT-323-TM/Authentication/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+
+
+namespace PROJECT_323_TM.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ PROJECT-323-TM/Authentication/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace PROJECT_323_TM.Authentication
+{
+    public class Response
+    {
+
+        public string Status { get; set; }
+        public string Message { get; set; }
+
+    }
+}
